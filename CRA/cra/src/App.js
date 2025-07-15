6bf83e613c66bfa5f143380efb70ee7a85703bdf
Stanislav <stasikrybcev@gmail.com>
2025-07-15 19:10:40 +0200

    Strict regime

--- CRA/cra/src/App.js
+++ CRA/cra/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, StrictMode } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -39,7 +39,9 @@ function Btn() {
 function App() {
   return (
     <div className="App">
-      <Header />
+      <StrictMode>
+        <Header />
+      </StrictMode>
       <Field />
       <Btn></Btn>
     </div>
