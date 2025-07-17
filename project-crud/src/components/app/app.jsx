48b225e6e33c2a64a53bdb903425b0b2492d334b
Stanislav <stasikrybcev@gmail.com>
2025-07-17 22:14:32 +0200

    State Immutability and Proper Events

--- project-crud/src/components/app/app.jsx
+++ project-crud/src/components/app/app.jsx
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import AppFilter from '../app-filter/app-filter';
 import SearchPanel from '../search-panel/search-panel';
@@ -5,25 +7,40 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: "John", salary: 800, increase: false, id: 1 },
+                { name: "Devid", salary: 300, increase: true, id: 2 },
+                { name: "Cbum", salary: 508, increase: false, id: 3 },
+            ]
+        }
+    }
 
-    const data = [
-        { name: "John", salary: 800, increase: false, id: 1 },
-        { name: "Devid", salary: 300, increase: true, id: 2 },
-        { name: "Cbum", salary: 508, increase: false, id: 3 },
-    ];
+    deleteItem = (id) => {
+        this.setState(({ data }) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-    return (
-        <div className="app">
-            <AppInfo />
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+                <EmployeesList data={this.state.data}
+                    onDelete={this.deleteItem} />
+                <EmployeesAddForm />
             </div>
-            <EmployeesList data={data} />
-            <EmployeesAddForm />
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
