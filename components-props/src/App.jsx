fa727d8e9acfe83a74088a5de60a0e2caa9dd28f
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-07-28 04:06:11 +0200

    What is ref and why is it needed

--- components-props/src/App.jsx
+++ components-props/src/App.jsx
@@ -1,160 +1,60 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import AutoLayoutExample from './BootstrapTest';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
-const EmpItem = styled.div`
-  padding: 20px;
-  margin-bottom: 15px;
-  border-radius: 5px;
-  box-shadow: 5px 5px 10px rgba(0,0,0, .8);
-  a {
-    display: block;
-    margin:10px 0 10px 0;
-    color: ${props => props.active ? "orange" : " black"};
+class Form extends Component {
+  // myRef = React.createRef();
+
+  // componentDidMount() {
+  //   this.myRef.current.focus();
+  // }
+
+  setInputRef = (elem) => {
+    this.myRef = elem;
   }
-  input{
-    display: block;
-    margin-top: 10px;
-  }
-`;
 
-const Header = styled.h2`
-  font-size: 22px;
-`;
-
-export const Button = styled.button`
-  display: block;
-  padding: 5px 15px;
-  background-color: gold;
-  border: 1px solid rgba(0,0,0, .8);
-  box-shadow: 5px 5px 10px rgba(0,0,0, .8)
-`;
-
-class WhoAmI extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      years: 19,
-      position: ''
+  focusFirst = () => {
+    if (this.myRef) {
+      this.myRef.focus();
     }
   }
 
-  nextYear = () => {
-    this.setState(state => ({
-      years: state.years + 1
-    }))
-  }
-
-  commitInputChanges = (e, color) => {
-    console.log(color);
-    this.setState({
-      position: e.target.value
-    })
-  }
-
   render() {
-    const { name, surname, link } = this.props;
-    const { position, years } = this.state;
     return (
-      <EmpItem active>
-        <Button onClick={this.nextYear}>+++</Button>
-        <Header>My name is {name}, surname - {surname}, age - {years},
-          position - {position}</Header>
-        <a href={link}>My profile</a>
-        <form>
-          <span>Enter your job title</span>
-          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+      <Container>
+        <form className="w-50 border mt-5 p-3 m-auto">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+            <input ref={this.setInputRef} type="email"
+              className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea onClick={this.focusFirst} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
         </form>
-      </EmpItem>
+      </Container>
     )
   }
 }
 
-const Wrapper = styled.div`
-  width: 600px;
-  margin: 80px auto 0 auto;
-`;
+class TextInput extends Component {
 
-const DynamicGreating = (props) => {
-  return (
-    <div className={'mb-3 p-3 border border-' + props.color}>
-      {
-        React.Children.map(props.children, child => {
-          return React.cloneElement(child, { className: 'shadow p-3 m-3 border rounded' })
-        })
-      }
-    </div>
-  )
-}
-
-const HelloGreating = () => {
-  return (
-    <div style={{ 'width': '500px', 'margin': '0 auto' }}>
-      <DynamicGreating color={'primary'}>
-        <h2>Hello world</h2>
-      </DynamicGreating>
-    </div>
-  )
-}
-
-const Message = (props) => {
-  return (
-    <h2>The counter is {props.counter}</h2>
-  )
-}
-
-class Counter extends Component {
-  state = {
-    counter: 0
-  }
-
-  changeCounter = () => {
-    this.setState(({ counter }) => ({
-      counter: counter + 1
-    }))
+  doSmth = () => {
+    console.log('Smth');
   }
 
   render() {
     return (
-      <>
-        <button
-          className={'btn btn-primary'}
-          onClick={this.changeCounter}>
-          Clikc me
-        </button>
-        {this.props.render(this.state.counter)}
-      </>
+      <input type="email"
+        className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
     )
   }
 }
 
 function App() {
   return (
-    <Wrapper>
-
-      <Counter render={counter => (
-        <Message counter={counter} />
-      )} />
-
-      <HelloGreating />
-      <AutoLayoutExample
-        left={
-          <DynamicGreating color={'primary'}>
-            <h2>This weel was hard</h2>
-            <h2>Hello world</h2>
-          </DynamicGreating>
-        }
-        right={
-          <DynamicGreating color={'primary'}>
-            <h2>Right</h2>
-          </DynamicGreating>
-        }
-      />
-
-      <WhoAmI name='Stasnislav' surname="Riabstev" link="facebook.com" />
-      <WhoAmI name='Max' surname="Bob" link="youtube.com" />
-    </Wrapper>
+    <Form />
   );
 }
 
--- components-props/src/index.jsx
+++ components-props/src/index.jsx
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Button } from './App';
-import styled from 'styled-components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const BigButton = styled(Button)`
-  margin: 0 auto;
-  width: 245px;
-  text-align: center;
-`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <BigButton as="a">HERE</BigButton>
   </React.StrictMode>
 );
