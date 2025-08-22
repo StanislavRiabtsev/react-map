e8cc8d08fbfbd089f5023a96a9290c30af645ba7
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-08-22 21:45:21 +0200

    s

--- components-props/src/App.jsx
+++ components-props/src/App.jsx
@@ -54,7 +54,7 @@ const Slider = ({ initial }) => {
             onClick={() => dispatch({ type: 'fast' })}>fast </button>
           <button
             className="btn btn-primary me-2"
-            onClick={(e) => dispatch({ type: 'custom', payload: +e.target.textContent })}>2000</button>
+            onClick={(e) => dispatch({ type: 'customы', payload: +e.target.textContent })}>2000</button>
         </div>
       </div>
     </Container>
