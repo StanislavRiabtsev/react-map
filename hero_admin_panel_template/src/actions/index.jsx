7a1ba1a5ba742e4d6a15e05e580b19062ea8f88c
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-01 19:02:02 +0200

    Redux Toolkit: createAction()

--- hero_admin_panel_template/src/actions/index.jsx
+++ hero_admin_panel_template/src/actions/index.jsx
@@ -1,22 +1,28 @@
-export const  fetchHeroes = (request) => (dispatch) => {
+import { createAction } from "@reduxjs/toolkit";
+
+export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
     request("http://localhost:3001/heroes")
         .then(data => dispatch(heroesFetched(data)))
         .catch(() => dispatch(heroesFetchingError()))
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+// export const heroesFetching = () => {
+//     return {
+//         type: 'HEROES_FETCHING'
+//     }
+// }
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+export const heroesFetching = createAction('HEROES_FETCHING');
+
+// export const heroesFetched = (heroes) => {
+//     return {
+//         type: 'HEROES_FETCHED',
+//         payload: heroes
+//     }
+// }
+
+export const heroesFetched = createAction('HEROES_FETCHED');
 
 export const heroesFetchingError = () => {
     return {
