93fa619881e9f22812b198c7d6c4dbd82fbd5da5
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-01 19:30:45 +0200

    Redux Toolkit: createReducer()

--- hero_admin_panel_template/src/actions/index.jsx
+++ hero_admin_panel_template/src/actions/index.jsx
@@ -7,28 +7,12 @@ export const fetchHeroes = (request) => (dispatch) => {
         .catch(() => dispatch(heroesFetchingError()))
 }
 
-// export const heroesFetching = () => {
-//     return {
-//         type: 'HEROES_FETCHING'
-//     }
-// }
-
 export const heroesFetching = createAction('HEROES_FETCHING');
-
-// export const heroesFetched = (heroes) => {
-//     return {
-//         type: 'HEROES_FETCHED',
-//         payload: heroes
-//     }
-// }
-
 export const heroesFetched = createAction('HEROES_FETCHED');
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroCreated = createAction('HERO_CREATED');
+export const heroDeleted = createAction('HERO_DELETED');
 
 export const filtersFetching = () => {
     return {
@@ -53,28 +37,5 @@ export const activeFilterChanged = (filter) => {
     return {
         type: 'ACTIVE_FILTER_CHANGED',
         payload: filter
-    }
-}
-
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000)
-// }
-
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
-
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
     }
 }
