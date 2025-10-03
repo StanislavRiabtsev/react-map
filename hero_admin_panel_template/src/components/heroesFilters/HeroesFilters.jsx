c2e058af30ce51bdfcdc8989df4c9dcf86fed6c8
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-03 16:49:30 +0200

    Redux Toolkit: createSlice() Filters

--- hero_admin_panel_template/src/components/heroesFilters/HeroesFilters.jsx
+++ hero_admin_panel_template/src/components/heroesFilters/HeroesFilters.jsx
@@ -1,27 +1,26 @@
-import { useHttp } from '../../hooks/http.hook';
+import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from '../../actions';
+import { fetchFilters } from '../../actions';
+import { filtersChanged } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
+
 const HeroesFilters = () => {
 
-    const { filters, filtersLoadingStatus, activeFilter } = useSelector(state => state.filters);
+    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
     const dispatch = useDispatch();
-    const { request } = useHttp();
+    const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(filtersFetching());
-        request("http://localhost:3001/filters")
-            .then(data => dispatch(filtersFetched(data)))
-            .catch(() => dispatch(filtersFetchingError()))
+        dispatch(fetchFilters(request));
 
         // eslint-disable-next-line
     }, []);
 
     if (filtersLoadingStatus === "loading") {
-        return <Spinner />;
+        return <Spinner/>;
     } else if (filtersLoadingStatus === "error") {
         return <h5 className="text-center mt-5">Loading error</h5>
     }
@@ -31,18 +30,18 @@ const HeroesFilters = () => {
             return <h5 className="text-center mt-5">No filters found</h5>
         }
 
-        return arr.map(({ name, className, label }) => {
+        return arr.map(({name, className, label}) => {
 
             const btnClass = classNames('btn', className, {
                 'active': name === activeFilter
             });
-
-            return <button
-                key={name}
-                id={name}
-                className={btnClass}
-                onClick={() => dispatch(activeFilterChanged(name))}
-            >{label}</button>
+            
+            return <button 
+                        key={name} 
+                        id={name} 
+                        className={btnClass}
+                        onClick={() => dispatch(filtersChanged(name))}
+                        >{label}</button>
         })
     }
 
