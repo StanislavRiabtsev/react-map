f3c40b50d212a300050d8cde0ab17e97a9481173
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-05 20:13:06 +0200

    Redux Toolkit: createEntityAdapter()

--- hero_admin_panel_template/src/components/heroesList/heroesSlice.jsx
+++ hero_admin_panel_template/src/components/heroesList/heroesSlice.jsx
@@ -1,10 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
 import { useHttp } from '../../hooks/http.hook';
 
-const initialState = {
-    heroes: [],
+const heroesAdapter = createEntityAdapter();
+
+const initialState = heroesAdapter.getInitialState({
     heroesLoadingStatus: 'idle',
-}
+});
+
+// const initialState = {
+//     heroes: [],
+//     heroesLoadingStatus: 'idle',
+// }
 
 export const fetchHeroes = createAsyncThunk(
     'heroes/fetchHeroes',
@@ -19,24 +25,27 @@ const heroesSlice = createSlice({
     initialState,
     reducers: {
         heroCreated: (state, action) => {
-            state.heroes.push(action.payload);
+            heroesAdapter.addOne(state, action.payload);
         },
         heroDeleted: (state, action) => {
-            state.heroes = state.heroes.filter(item => item.id !== action.payload);
+            heroesAdapter.removeOne(state, action.payload);
         }
     },
     extraReducers: (builder) => {
         builder
-            .addCase(fetchHeroes.pending, (state) => {state.heroesLoadingStatus = 'loading';})
-            .addCase(fetchHeroes.fulfilled, (state, action) => {state.heroesLoadingStatus = 'idle';state.heroes = action.payload;})
-            .addCase(fetchHeroes.rejected, (state) => {state.heroesLoadingStatus = 'error';})
-            .addDefaultCase(() => {})
+            .addCase(fetchHeroes.pending, (state) => { state.heroesLoadingStatus = 'loading'; })
+            .addCase(fetchHeroes.fulfilled, (state, action) => { state.heroesLoadingStatus = 'idle'; heroesAdapter.setAll(state, action.payload) })
+            .addCase(fetchHeroes.rejected, (state) => { state.heroesLoadingStatus = 'error'; })
+            .addDefaultCase(() => { })
     }
 });
 
 const { actions, reducer } = heroesSlice;
 
 export default reducer;
+
+export const { selectAll } = heroesAdapter.getSelectors(state => state.heroes);
+
 export const {
     heroesFetching,
     heroesFetched,
