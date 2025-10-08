19e1c751532c6f2cc4036a20f3b4e9ccfab3599a
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-08 21:31:53 +0200

    npm, npx

--- hook/src/index.jsx
+++ hook/src/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import HelloWorldNPM from 'hello-world-npm';
+
+console.log(HelloWorldNPM.helloWorld());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
