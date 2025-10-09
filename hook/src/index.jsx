cf55c6be72f6a68ae296d914412fca28fe99765e
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-09 20:44:28 +0200

    jsx: introduction

--- hook/src/index.jsx
+++ hook/src/index.jsx
@@ -2,21 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import { HELLO } from './Folder/hello';
-import { HOOP } from './Folder/index';
-import { TEST1, TEST2 } from './test';
-import Dog from './Dog.jpg';
-import './index.css';
-
-console.log(TEST1, TEST2);
-
-console.log(HELLO);
-console.log(HOOP);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode>
-    <div className='back'>
-        <img src={Dog} alt="dog" />
+root.render(
+    <React.StrictMode>
         <App />
-    </div>
-</React.StrictMode>);
+    </React.StrictMode>);
--- hook/src/test.jsx
+++
@@ -1,4 +0,0 @@
-const TEST1 = 'test1';
-const TEST2 = 'test2';
-
-export { TEST1, TEST2 };
