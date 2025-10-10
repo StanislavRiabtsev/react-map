17c0fa25f15b41c928a0d97edd9b42c3100e8d95
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-10 23:57:40 +0200

    jsx: conditions, functions, ternary operator

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import './index.css';
 
 const App = () => {
-    return <h1>Hello World</h1>;
+    const name = 'Stanislav';
+    const age = 36;
+
+    let atr;
+
+    if (age === 36) {
+        atr = 'good';
+    } else {
+        atr = 'bad';
+    }
+
+    function test() {
+        return 'hello';
+    }
+
+    return <h1 className={atr}>
+        Hello World {name}, you are {age} years old.
+        <div className="div">
+            {age === 36 ? 'You are good' : 'You are bad'}
+            <br />
+            {test()}
+        </div>
+    </h1>;
 };
 
 export default App;
