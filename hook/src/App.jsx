6351994b34d5c07adcf930fffecabaff76320097
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-14 20:41:03 +0200

    onClick, onChange events

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -1,66 +1,25 @@
 import React from "react";
-import UserName from "./UserName";
-import Button from "./Button";
-import Description from "./Description";
-
 import './index.css';
 
-let USERS = [
-    { id: 1, name: 'Alex', surname: 'Smith', age: 25, passportId: 123 },
-    { id: 2, name: 'Bob', surname: 'Johnson', age: 30, passportId: 456 },
-    { id: 3, name: 'Arnold', surname: 'Williams', age: 15, passportId: 123 },
-    { id: 4, name: 'Felix', surname: 'Jones', age: 40, passportId: 101112 },
-]
 
 const App = () => {
-    const changeName = (id, newName) => {
-        USERS.map((user) => {
-            if (user.id === id) {
-                return {
-                    id: user.id,
-                    name: newName,
-                    surname: user.surname,
-                    age: user.age,
-                    passportId: user.passportId
-                }
-            } else {
-                return user;
-            }
-        });
+    const onClickHandler = (event) => {
+        console.log('click', event);
     }
-    const data = [];
-    USERS.forEach((user, index) => {
-        const key = generateKey(user);
-        data.push(
 
-            <div key={key}>
-                <UserName
-                    userName={user.name}
-                    userSurname={user.surname}
-                    userAge={user.age}
-                    changeName={changeName}
-                />
-                {/* {user.age > 20 ?
-                    (<p className="">{user.name}
-                        <button>Show Details</button>
-                    </p>)
-                    :
-                    (<p className="">{user.name} {user.surname}</p>)
-                } */}
-            </div>
-        );
-    });
+    const onChangeHandler = (event) => {
+        console.log('change', event.target.value);
+    }
     return (
-        <div className="">
-            {/* {data} */}
-            <Button />
-            <Description />
-        </div>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            console.log("Submit");
+        }}>
+            Test
+            <input type="text" onChange={onChangeHandler} />
+            <button onClick={onClickHandler}>Click Me</button>
+        </form>
     );
 };
-
-function generateKey(user) {
-    return `${user.name}-${user.surname}-${user.age}-${user.passportId}`;
-}
 
 export default App;
