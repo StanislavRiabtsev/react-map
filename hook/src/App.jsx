1a7a5797adef7b1db815361ee74e5348cf59f45b
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-10-14 20:27:34 +0200

    props - passing data between components

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -1,31 +1,65 @@
 import React from "react";
+import UserName from "./UserName";
+import Button from "./Button";
+import Description from "./Description";
 
 import './index.css';
 
-const USERS = [
+let USERS = [
     { id: 1, name: 'Alex', surname: 'Smith', age: 25, passportId: 123 },
     { id: 2, name: 'Bob', surname: 'Johnson', age: 30, passportId: 456 },
-    { id: 3, name: 'Arnold', surname: 'Williams', age: 35, passportId: 123 },
+    { id: 3, name: 'Arnold', surname: 'Williams', age: 15, passportId: 123 },
     { id: 4, name: 'Felix', surname: 'Jones', age: 40, passportId: 101112 },
 ]
 
 const App = () => {
+    const changeName = (id, newName) => {
+        USERS.map((user) => {
+            if (user.id === id) {
+                return {
+                    id: user.id,
+                    name: newName,
+                    surname: user.surname,
+                    age: user.age,
+                    passportId: user.passportId
+                }
+            } else {
+                return user;
+            }
+        });
+    }
+    const data = [];
+    USERS.forEach((user, index) => {
+        const key = generateKey(user);
+        data.push(
+
+            <div key={key}>
+                <UserName
+                    userName={user.name}
+                    userSurname={user.surname}
+                    userAge={user.age}
+                    changeName={changeName}
+                />
+                {/* {user.age > 20 ?
+                    (<p className="">{user.name}
+                        <button>Show Details</button>
+                    </p>)
+                    :
+                    (<p className="">{user.name} {user.surname}</p>)
+                } */}
+            </div>
+        );
+    });
     return (
-        <div>
-            {USERS.map((user, index) => {
-                const key = generateKey(user);
-                console.log(key);
-                return (
-                    <div key={key}>
-                        <p>{user.name}</p>
-                    </div>
-                )
-            })}
+        <div className="">
+            {/* {data} */}
+            <Button />
+            <Description />
         </div>
     );
 };
 
-function genereateKey(user) {
+function generateKey(user) {
     return `${user.name}-${user.surname}-${user.age}-${user.passportId}`;
 }
 
---
+++ hook/src/Description.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "./Button";
+
+const Description = () => {
+    const dataFromServer = '...';
+    return (
+        <Button dataFromServer={dataFromServer} />
+    );
+};
+
+export default Description;
